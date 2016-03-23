9be500724604ab76d1021df1759f788876a4a584
Kirill Nesmeyanov <nesk@xakep.ru>
2016-03-24 01:15:02 +0300

    Update ApiIterator.php

--- src/Support/ApiIterator.php
+++ src/Support/ApiIterator.php
@@ -8,3 +8,45 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+namespace Gitter\Support;
+
+/**
+ * Class ApiIterator
+ * @package Gitter
+ */
+class ApiIterator implements \IteratorAggregate
+{
+    /**
+     * @var \Closure
+     */
+    private $request;
+
+    /**
+     * @var int
+     */
+    private $page = 0;
+
+    /**
+     * ApiIterator constructor.
+     * @param \Closure $request
+     */
+    public function __construct(\Closure $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * @return \Generator
+     */
+    public function getIterator() : \Generator
+    {
+        do {
+            $result = ($this->request)($this->page++);
+            if (!count($result)) { break; }
+
+            foreach ($result as $item) {
+                yield $item;
+            }
+        } while(true);
+    }
+}
